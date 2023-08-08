35c3374433b95ac96b3d088aa61a45f62024cdb5
musabnaik <musab944@gmail.com>
2023-08-07 21:47:56 -0400

    added treemap graph to show types of electricity generation

--- app.py
+++ app.py
@@ -8,11 +8,8 @@ APP_TITLE = 'Electricity Generation in Canada'
 APP_SUB_TITLE = 'Source: Stats Canada'
 
 def display_map(df, year,prov):
-    df=df[df['year']==year]
-    if prov in df['GEO'].unique():
-        df=df[df['GEO']==prov]
-    df = df.groupby(['GEO'])['VALUE'].sum().reset_index()
     #st.write(df)
+    df = df.groupby(['GEO'])['VALUE'].sum().reset_index()
     with open('./Canada_ShapeFile.geojson', 'r') as geo:
         mp = json.load(geo)
 
@@ -22,7 +19,7 @@ def display_map(df, year,prov):
                         #range_color=(0, 14223942),
                         mapbox_style = 'open-street-map',
                         center=dict(lat=59.959354, lon=-101.990312),
-                        zoom=2.75,opacity=0.5,width=1000, height=500
+                        zoom=2.55,opacity=0.5,width=1000, height=500
                         #colorbar={"orientation": "h", "x": 0.5, "yanchor": "middle", "y": 0.1}
                     )
 
@@ -33,10 +30,21 @@ def display_map(df, year,prov):
                     x=0.01,  # Adjust the x-coordinate (0.0 to 1.0) for horizontal position
                     y=0.5,  # Adjust the y-coordinate (0.0 to 1.0) for vertical position
                     len=0.7,  # Adjust the length of the color bar
-                    ))
+                    )
+                )
 
     #fig.show()
     st.plotly_chart(fig,use_container_width=False)
+
+def display_treemap(df):
+    df = df.groupby(['Type of electricity generation'])['VALUE'].sum().reset_index()
+    fig = px.treemap(df, 
+                 names='Type of electricity generation', 
+                 parents=['']*(df.shape[0]), 
+                 values='VALUE',width=1000, height=500)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    st.plotly_chart(fig,use_container_width=False)
+
 
 def main():
     st.set_page_config(layout="wide",page_title=APP_TITLE)
@@ -61,8 +69,13 @@ def main():
 
     year = st.sidebar.selectbox('Year',year_list)
     prov = st.sidebar.selectbox('Provience',prov_list)
-    display_map(df, year, prov)
 
+    df=df[df['year']==year]
+    if prov in df['GEO'].unique():
+        df=df[df['GEO']==prov]
+
+    display_map(df, year, prov)
+    display_treemap(df)
 
 if __name__ == '__main__':
     main()
